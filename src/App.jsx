4243390aaf92cbb08128a37b986cc38142950b3d
mateusz.kwiatkowski@billennium.com <mateusz.kwiatkowski1155@gmail.com>
2021-08-31 14:25:14 +0200

    add messenger

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import MessengerCustomerChat from 'react-messenger-customer-chat';
 import { Header } from './components/Header/Header';
 import { Wedding } from './pages/wedding/Wedding';
 import { Home } from './pages/Home/Home';
 import { Navigation } from './components/Navigation/Navigation';
 import { Footer } from './components/Footer/Footer';
 import { ServiceSubPage } from './pages/ServiceSubPage/ServiceSubPage';
-import MessengerCustomerChat from 'react-messenger-customer-chat';
 import './styles/Global.scss';
 
-console.log(MessengerCustomerChat);
 export const App = () => (
   <Router>
     <Navigation />
@@ -20,6 +19,10 @@ export const App = () => (
       <Route path="/event" component={ServiceSubPage} />
       <Route component={Home} />
     </Switch>
+    <MessengerCustomerChat 
+      pageId="499290697138600"
+      appId="385967879567945"
+    />
     <Footer />
   </Router>
 );
