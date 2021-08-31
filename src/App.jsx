adc7b5b4449064051e483c78aac19583f6046e60
MatYou <matyou.kwiatkowsky@gmail.com>
2021-08-31 15:44:02 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import MessengerCustomerChat from 'react-messenger-customer-chat';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Header } from './components/Header/Header';
 import { Wedding } from './pages/wedding/Wedding';
 import { Home } from './pages/Home/Home';
@@ -18,10 +18,7 @@ export const App = () => (
       <Route path="/wedding" component={Wedding} />
       <Route component={Home} />
     </Switch>
-    <MessengerCustomerChat 
-      pageId="499290697138600"
-      appId="385967879567945"
-    />
+    <MessengerCustomerChat pageId="499290697138600" appId="385967879567945" />
     <Footer />
   </Router>
 );
